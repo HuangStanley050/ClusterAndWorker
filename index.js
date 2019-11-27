df2f85e8dc633c31f431470d2bb4223972efdcb3
Stanley Huang <huang.stanley050@gmail.com>
2019-11-27 19:47:44 +1100

    start to benchmark clustering with ab

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-const express = require("express");
-const app = express();
+process.env.UV_THREADPOOL_SIZE = 1;
+const cluster = require("cluster");
 
-function doWork(duration) {
-  const start = Date.now();
-  while (Date.now() - start < duration) {}
+//ask is the file being exectuted in master mode?
+if (cluster.isMaster) {
+  //make index js exectute again in child mode
+  cluster.fork();
+  // cluster.fork();
+  // cluster.fork();
+  // cluster.fork();
+} else {
+  // am a child, act like a server
+  const express = require("express");
+  const app = express();
+  const crypto = require("crypto");
+  app.get("/", (req, res) => {
+    crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+      res.send("Hello world");
+    });
+  });
+  app.get("/fast", (req, res) => {
+    res.send("this is fast");
+  });
+  app.listen(3000, () => console.log("server running"));
 }
-
-app.get("/", (req, res) => {
-  doWork(5000); //blocking in the event loop
-  res.send("hello");
-});
-
-app.listen(3000, () => console.log("server running"));
